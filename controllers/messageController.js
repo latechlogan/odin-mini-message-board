d7e152bff36f071ad3dd0ef1e39376a92326dae5
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-12-29 16:59:08 -0600

    feat: add message detail view with ID routing

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,15 +1,19 @@
 const messages = [
   {
+    id: 0,
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: 1,
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
   },
 ];
+
+let nextId = 2;
 
 exports.getMessages = (req, res) => {
   res.render("index", { messages: messages });
@@ -21,9 +25,16 @@ exports.getNewMessageForm = (req, res) => {
 
 exports.postNewMessage = (req, res) => {
   messages.push({
+    id: nextId++,
     text: req.body.message,
     user: req.body.user,
     added: new Date(),
   });
   res.redirect("/");
+};
+
+exports.getMessageDetails = (req, res) => {
+  const messageId = parseInt(req.params.id);
+  const message = messages[messageId];
+  res.render("details", { message: message });
 };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,5 +5,6 @@ const messageController = require("../controllers/messageController");
 router.get("/", messageController.getMessages);
 router.get("/new", messageController.getNewMessageForm);
 router.post("/new", messageController.postNewMessage);
+router.get("/details/:id", messageController.getMessageDetails);
 
 module.exports = router;
