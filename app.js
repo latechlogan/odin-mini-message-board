98eb782d8a285d0118385d0b9464661214409854
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-12-29 21:57:36 -0600

    style: add Tailwind CSS and shared EJS head partial

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const indexRouter = require("./routes/indexRouter");
 
 const PORT = 3000;
@@ -7,6 +8,7 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
